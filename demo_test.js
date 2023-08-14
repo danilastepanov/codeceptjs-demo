21bd0a6695fcf1a59241bce55c716a4c94fb9a1c
Danila Stepanov <dialon.gm@gmail.com>
2023-08-14 18:37:50 +0300

    test two Todo creation

--- demo_test.js
+++ demo_test.js
@@ -1,3 +1,13 @@
 Feature("demo");
 
-Scenario("test something", ({ I }) => {});
+Scenario("test two Todo creation", ({ I }) => {
+    I.amOnPage("http://todomvc.com/examples/react/");
+    I.fillField("What needs to be done?", "codeceptjs-demo1");
+    I.pressKey("Enter");
+    I.see("codeceptjs-demo1", ".todo-list");
+    I.see("1 item left", ".todo-count");
+    I.fillField("What needs to be done?", "codeceptjs-demo2");
+    I.pressKey("Enter");
+    I.see("codeceptjs-demo2", ".todo-list");
+    I.see("2 items left", ".todo-count");
+});
